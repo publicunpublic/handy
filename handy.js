f244cc6c4ab6b2e4abcae8e6b055c8413b1f70cd
publicunpublic <92448690+publicunpublic@users.noreply.github.com>
2023-05-13 15:22:47 +0200

    Update handy.js

--- handy.js
+++ handy.js
@@ -1,1 +1,79 @@
+<script>
+// Define variables
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+var imgCount = 11; // Number of images to use
+var imgPaths = []; // Array to store image paths
+var imgIndex = 0; // Current image index
+var trail = []; // Array to store trail positions
+var trailLength = 10; // Number of trail positions to store
+var trailSize = 50; // Size of trail image
+var mouseX, mouseY; // Mouse coordinates
 
+// Add canvas to page
+canvas.style.position = "fixed";
+canvas.style.top = "0";
+canvas.style.left = "0";
+canvas.style.zIndex = "-1";
+document.body.appendChild(canvas);
+
+// Load images
+for (var i = 0; i < imgCount; i++) {
+imgPaths.push("https://d2w9rnfcy7mm78.cloudfront.net/15351377/original_3280f0a20a9ec2e9ae6c0c20f45f9db5.jpg?1645926648?bc=0");
+}
+var images = [];
+var loadedImages = 0;
+for (var i = 0; i < imgCount; i++) {
+  images[i] = new Image();
+  images[i].onload = function() {
+    loadedImages++;
+    if (loadedImages == imgCount) {
+      requestAnimationFrame(draw);
+    }
+  };
+  images[i].src = imgPaths[i];
+}
+
+// Handle mouse movements
+document.addEventListener("mousemove", function(e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+// Handle touch events
+document.addEventListener("touchmove", function(e) {
+  e.preventDefault();
+  var touch = e.changedTouches[0];
+  mouseX = touch.clientX;
+  mouseY = touch.clientY;
+});
+
+// Draw function
+function draw() {
+  // Clear canvas
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Add current position to trail
+  trail.push({
+    x: mouseX,
+    y: mouseY
+  });
+  if (trail.length > trailLength) {
+    trail.shift();
+  }
+
+  // Draw trail
+  for (var i = 0; i < trail.length; i++) {
+    var img = images[(imgIndex + i) % imgCount];
+    ctx.drawImage(img, trail[i].x - trailSize / 2, trail[i].y - trailSize / 2, trailSize, trailSize);
+  }
+
+  // Update image index
+  imgIndex = (imgIndex + 1) % imgCount;
+
+  // Request next frame
+  requestAnimationFrame(draw);
+}
+</script>
